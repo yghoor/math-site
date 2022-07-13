d139eddf140438e996f96474473d5c37cf1cbbf4
Yahya Ghoor <ghooryahya@gmail.com>
2022-07-13 21:33:13 +0200

    Import calculator stylesheet to component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
+import '../stylesheets/Calculator.css';
 
 const Display = () => (
   <div className="calculator-display">
