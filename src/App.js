6a82b4b98acec22238a3d82496f5ce8bb17a7aed
Yahya Ghoor <ghooryahya@gmail.com>
2022-07-13 21:34:10 +0200

    Render calculator inside 'App' div

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import Calculator from './components/Calculator.js';
 
 function App() {
-
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
