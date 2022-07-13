e8ced47c44be470010cd93329ed562b170efd9d9
Yahya Ghoor <ghooryahya@gmail.com>
2022-07-13 21:15:27 +0200

    Create function to return calculator buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,3 +5,28 @@ const Display = () => (
     <div className="display-value">0</div>
   </div>
 );
+
+const Buttons = () => (
+  <div className="buttons">
+    <button className="button value-button">AC</button>
+    <button className="button value-button">+/-</button>
+    <button className="button value-button">%</button>
+    <button className="button operator-button">÷</button>
+    <button className="button value-button">7</button>
+    <button className="button value-button">8</button>
+    <button className="button value-button">9</button>
+    <button className="button operator-button">×</button>
+    <button className="button value-button">4</button>
+    <button className="button value-button">5</button>
+    <button className="button value-button">6</button>
+    <button className="button operator-button">-</button>
+    <button className="button value-button">1</button>
+    <button className="button value-button">2</button>
+    <button className="button value-button">3</button>
+    <button className="button operator-button">+</button>
+    <button className="button value-button zero-button">0</button>
+    <button className="button value-button">.</button>
+    <button className="button operator-button">=</button>
+  </div>
+);
+
