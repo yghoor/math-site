1f47fc70f370ecd6e11ab45fe1b1d09579726865
Yahya Ghoor <ghooryahya@gmail.com>
2022-07-25 22:06:52 +0200

    Import HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
