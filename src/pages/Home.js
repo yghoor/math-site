ea9bea1dfee37d74e4289e6c24b2cb7b883ea448
Yahya Ghoor <ghooryahya@gmail.com>
2022-07-25 22:24:48 +0200

    Create home page component

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+const Home = () => (
+  <div className="home-page">
+    <h1>Welcome to our page!</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet quod fugit incidunt,
+      possimus ea voluptatibus. Error tenetur quis ullam minus, perferendis dolore ipsam ut dolor
+      ipsa quo commodi nulla? Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet
+      quod fugit incidunt, possimus ea voluptatibus. Error tenetur quis ullam minus, perferendis
+      dolore ipsam ut dolor ipsa quo commodi nulla? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Illum amet quod fugit incidunt, possimus ea voluptatibus. Error tenetur
+      quis ullam minus, perferendis dolore ipsam ut dolor ipsa quo commodi nulla?
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet quod fugit incidunt,
+      possimus ea voluptatibus. Error tenetur quis ullam minus, perferendis dolore ipsam ut dolor
+      ipsa quo commodi nulla? Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet
+      quod fugit incidunt, possimus ea voluptatibus. Error tenetur quis ullam minus, perferendis
+      dolore ipsam ut dolor ipsa quo commodi nulla? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Illum amet quod fugit incidunt, possimus ea voluptatibus. Error tenetur
+      quis ullam minus, perferendis dolore ipsam ut dolor ipsa quo commodi nulla?
+    </p>
+  </div>
+);
+
+export default Home;
