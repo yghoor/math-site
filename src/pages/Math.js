8505662e86ed551265c77d9f54ed02bff8043abb
Yahya Ghoor <ghooryahya@gmail.com>
2022-07-25 22:25:59 +0200

    Create math page component
    And move calculator render here

---
+++ src/pages/Math.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import Calculator from '../components/Calculator.js';
+
+const Math = () => (
+    <div className="math-page">
+      <h1>Let's do some math!</h1>
+      <Calculator />
+    </div>
+
+);
+
+export default Math;
