f3ff90e400b97b0a712d4b36cebbe36594a95f17
Yahya Ghoor <ghooryahya@gmail.com>
2022-07-25 22:22:32 +0200

    Create page navbar

--- src/App.js
+++ src/App.js
@@ -11,3 +11,26 @@ function App() {
 }
 
 export default App;
+
+const Navbar = () => (
+
+  <header>
+    <nav className="page-navbar">
+      <h1>Math Magicians</h1>
+
+      <ul className="nav-links">
+        <li>
+          <NavLink to="/" activeclassname="active" >Home</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/math" activeclassname="active" >Calculator</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/quote" activeclassname="active" >Quote</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
